60fd0020c810c009a36566088af0806040589869
Håkon Malmedal <hmalmedal@gmail.com>
2013-09-02 16:36:00 +0200

    Data for Stoltenberg II

---
+++ minister.R
@@ -0,0 +1,11 @@
+library(lubridate)
+regjering <- read.csv("regjering.csv",
+                      colClasses=c("character",
+                                   "Date",
+                                   "Date",
+                                   "integer",
+                                   "factor"))
+regjering[is.na(regjering)] <- today()
+regjering$Dager <- as.numeric(regjering$Sluttdato - regjering$Startdato)
+regjering.fit  <- survfit(with(regjering, Surv(Dager, Avskjed)) ~ 1)
+plot(regjering.fit)
