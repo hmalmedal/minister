57eaf9f5b6872496d18a4ef8d48ac10741d9284e
Håkon Malmedal <hmalmedal@gmail.com>
2015-06-22 12:19:46 +0200

    Velg regjeringer

--- app.R
+++ app.R
@@ -1,24 +1,54 @@
 library(readr)
 library(dplyr)
 library(lubridate)
+
 regjering <- read_csv("regjering.csv") %>%
   mutate(Sluttdato = replace(Sluttdato, is.na(Sluttdato), today("Europe/Oslo")),
          Dager = as.numeric(Sluttdato - Startdato),
          År = decimal_date(Sluttdato) - decimal_date(Startdato))
 
+regjeringer <- unique(regjering$Regjering)
+
 library(survival)
 library(broom)
+library(tidyr)
 library(ggvis)
+
 server <- function(input, output) {
   input_opacity <- reactive(input$opacity)
-  survfit(Surv(År, Avskjed) ~ 1, data = regjering) %>%
-    tidy() %>%
-    bind_rows(summarise(., time = 0, estimate = 1,
-                        conf.high = 1, conf.low = 1)) %>%
-    arrange(time) %>%
+
+  regjeringsdata <- reactive({
+    i <- which(regjering$Regjering %in% input$valgteregjeringer)
+    if (length(i) == 0) {
+      regjering$Regjering <- "Alle regjeringer"
+      regjering
+    } else {
+      regjering[i, ]
+    }
+  })
+
+  regjering_survfit <- reactive({
+    r <- regjeringsdata()$Regjering[1]
+
+    s <- survfit(Surv(År, Avskjed) ~ Regjering, data = regjeringsdata()) %>%
+      tidy()
+
+    if (is.null(s$strata))
+      s$strata <- paste0("Regjering=", r)
+
+    s %>% group_by(strata) %>%
+      bind_rows(summarise(., time = 0, estimate = 1,
+                          conf.high = 1, conf.low = 1)) %>%
+      arrange(strata, time) %>%
+      separate(strata, c("key", "Regjering"), "=")
+  })
+
+  regjering_survfit %>%
+    group_by(Regjering) %>%
     ggvis(x = ~time, interpolate := "step-after") %>%
-    layer_ribbons(y = ~conf.high, y2 = ~conf.low, opacity := input_opacity) %>%
-    layer_lines(y = ~estimate) %>%
+    layer_ribbons(y = ~conf.high, y2 = ~conf.low, opacity := input_opacity,
+                  fill = ~Regjering) %>%
+    layer_lines(y = ~estimate, stroke = ~Regjering) %>%
     scale_numeric("y", domain = c(0, 1)) %>%
     bind_shiny("r")
 }
@@ -27,6 +57,10 @@ ui <- fluidPage(
   titlePanel("Tid i regjering"),
   sidebarLayout(
     sidebarPanel(
+      selectInput(inputId = "valgteregjeringer",
+                  label = "Velg regjeringer",
+                  choices = regjeringer,
+                  multiple = TRUE),
       sliderInput("opacity", "Opasitet:",
                   min = 0, max = 1, value = 0.2)
     ),
